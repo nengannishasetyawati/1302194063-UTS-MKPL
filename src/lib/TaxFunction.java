5b438f016c6230fac2e5dcd226c48156ce63562a
nengannishasetyawati <55216332+nengannishasetyawati@users.noreply.github.com>
2022-04-20 23:46:33 +0700

    LongMethod-TaxFunction
    Memecah method calculateTax menjadi beberapa method dengan single operasi (lebih sederhana).

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -18,27 +18,28 @@ public class TaxFunction {
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
 		if (isMarried) {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
 		}else {
 			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
 		}
-		
+	}
+	public static void checkNumberOfMonthWorking(int numberOfMonthWorking){
+		if (numberOfMonthWorking > 12) {
+			System.err.println("More than 12 month working per year");
+		}
+	}
+	public static void numberOfChildren( int numberOfChildren){
+		if (numberOfChildren > 3) {
+			numberOfChildren = 3;
+		}
+	}
+	public static void taxMustPaid(int tax){	
 		if (tax < 0) {
 			return 0;
 		}else {
 			return tax;
-		}
-			 
+		}	 
 	}
 	
 }
